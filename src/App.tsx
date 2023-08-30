4917abc949a0d1e9501f8df1cbbf697bb38ed546
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-30 01:23:23 -0400

    Added mockups

--- src/App.tsx
+++ src/App.tsx
@@ -5,17 +5,18 @@ function App() {
 
   return (
     <>
-    <div id='content'>
+    <div id='topContent'>
       <Navbar/>
         <div id='titleWrapper'>
           <h1 id='title'>Web Design & Development For Small Businesses</h1>
           <button id='contactButtonTitle'>Work With Us</button>
         </div>
+        <img id='mockup' src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693372448/qntzs1n6cyxtwu3ofrql.png' alt='Display Site'/>
     </div>
     <div id='buffer'></div>
     <svg id='wave' viewBox="0 0 500 200">
-          <path d="M 0 30 C 150 100 280 0 500 20 L 500 0 L 0 0" fill="rgb(0, 0, 0)"></path>
-      </svg>
+      <path d="M 0 30 C 150 100 280 0 500 20 L 500 0 L 0 0" fill="rgb(0, 0, 0)"></path>
+    </svg>
     </>
   );
 }
