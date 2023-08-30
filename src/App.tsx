05b0a9c21bee151f9d456a3f22bf437400af653b
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-29 23:17:45 -0400

    Added neon wave

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,17 @@ function App() {
 
   return (
     <>
+    <div id='content'>
       <Navbar/>
-      <div id='titleWrapper'>
-        <h1 id='title'>Web Design & Development For Small Businesses</h1>
-        <button id='contactButtonTitle'>Work With Us</button>
-      </div>
+        <div id='titleWrapper'>
+          <h1 id='title'>Web Design & Development For Small Businesses</h1>
+          <button id='contactButtonTitle'>Work With Us</button>
+        </div>
+    </div>
+    <div id='buffer'></div>
+    <svg id='wave' viewBox="0 0 500 200">
+          <path d="M 0 30 C 150 100 280 0 500 20 L 500 0 L 0 0" fill="rgb(0, 0, 0)"></path>
+      </svg>
     </>
   );
 }
