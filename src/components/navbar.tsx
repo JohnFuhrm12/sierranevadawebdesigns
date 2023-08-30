661bd8a09731688fa6c742f771a825992da47a29
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-08-30 01:50:33 -0400

    Mockup flicker and logo filler

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -4,9 +4,12 @@ function Navbar () {
     return (
         <>
             <div id='wrapper'>
-                <div id='navTitleWrapper'>
-                    <h2 className='titleItem'>Sierra Nevada</h2>
-                    <h2 className='titleItem'>Web Designs</h2>
+                <div id='navTitleLogoWrapper'>
+                    <img id='logo' src='=' alt=''/>
+                    <div id='navTitleWrapper'>
+                        <h2 className='titleItem'>Sierra Nevada</h2>
+                        <h2 className='titleItem'>Web Designs</h2>
+                    </div>
                 </div>
                 <div id='navItemsWrapper'>
                     <h2 className='navItem'>Home</h2>
