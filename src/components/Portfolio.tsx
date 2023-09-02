884fe5fe903aadab201f0d3d4e4aa9c5c3fadc57
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-01 22:03:21 -0400

    Added descriptions for portfolio pieces

--- src/components/Portfolio.tsx
+++ src/components/Portfolio.tsx
@@ -14,13 +14,13 @@ function Portfolio() {
                     </div>
                     <div className='contentBlockTextPortfolio'>
                         <h2 className='subTitlePage'><span className='titleBlue'>JF</span>Aquatics</h2>
-                        <p className='infoParaLarge'>We will solve any problems that you run into with the site, make any required changes, and continue to monitor and optimize SEO and traffic, all free of charge for the first month after launch.</p>
+                        <p className='infoParaLarge'>A reefing e-commerce site selling fish, corals, and tanks. Firebase was used as a back-end to store all of the products. The site is integrated with paypal to easily conduct e-commerce.</p>
                     </div>
                 </div>
                 <div className='contentWrapperPortfolio'>
                     <div className='contentBlockTextPortfolio'>
                         <h2 className='subTitlePage'><span className='titleBlue'>Hawai'i Surf</span>Surf</h2>
-                        <p className='infoParaLarge'>We will solve any problems that you run into with the site, make any required changes, and continue to monitor and optimize SEO and traffic, all free of charge for the first month after launch.</p>
+                        <p className='infoParaLarge'>A surf and weather forecast application centered around Hawai'i. The site draws buoy data from around the islands and makes use of an algorithm to predict wave heights and conditions. Users can login and add their favorite surf spots to their account to track.</p>
                     </div>
                     <div className='contentBlockImgPortfolio'>
                         <img className='portfolioMockupImg' src='https://res.cloudinary.com/dvmw658s9/image/upload/v1693463575/mammqpa0fuijhg960yvf.png'/>
@@ -32,7 +32,7 @@ function Portfolio() {
                     </div>
                     <div className='contentBlockTextPortfolio'>
                         <h2 className='subTitlePage'><span className='titleBlue'>Dharma</span>Pastelería</h2>
-                        <p className='infoParaLarge'>We will solve any problems that you run into with the site, make any required changes, and continue to monitor and optimize SEO and traffic, all free of charge for the first month after launch.</p>
+                        <p className='infoParaLarge'>A pastry and bakery business in Buenos Aires, Argentina. Firebase was incorporated as a back-end along with a client-side admin page to easily update products. The site is integrated with paypal to easily conduct e-commerce.</p>
                     </div>
                 </div>
             </div>
