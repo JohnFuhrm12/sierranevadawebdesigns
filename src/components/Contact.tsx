a88ed7128887bf15527ba05eea408b8a19c919ba
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-03 00:53:35 -0400

    Added formspree to contact form

--- src/components/Contact.tsx
+++ src/components/Contact.tsx
@@ -1,6 +1,15 @@
 import '../styles/contact.css';
+import { useForm, ValidationError } from '@formspree/react';
 
 function Contact() {
+
+    const [state, handleSubmit] = useForm("xvolwzaq");
+    if (state.succeeded) {
+        return (
+            <p className='successMessage'>Thank you, we will get back to you shortly!</p>
+        )
+    }
+
     return (
         <>
         <div id='contactBackground'>
@@ -12,13 +21,14 @@ function Contact() {
                 <p className='infoPara'>Send us a call at +1 (609)-351-0536</p>
             </div>
             <div id='contactFormBackground'>
-                <form id='contactForm'>
+                <form id='contactForm' onSubmit={handleSubmit}>
+                    <ValidationError prefix="Email" field="email" errors={state.errors}/>
                     <input type='text' name='name' placeholder='Full Name' required/>
                     <input type='text' name='business' placeholder='Business Name' required/>
                     <input type='text' name='phone' placeholder='+X (XXX)-(XXX)-(XXXX)' required/>
                     <input type='text' name='email' placeholder='myname@gmail.com' required/>
-                    <textarea placeholder='Send us a message!' required/>
-                    <button id='contactSubmitButton'>Send Message</button>
+                    <textarea name='message' placeholder='Send us a message!' required/>
+                    <button id='contactSubmitButton' type='submit' disabled={state.submitting}>Send Message</button>
                 </form>
             </div>
           </div>
