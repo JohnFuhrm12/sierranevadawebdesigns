925030196f34fe6e3394e6e22630716091cf357a
JohnFuhrm12 <61069716+JohnFuhrm12@users.noreply.github.com>
2023-09-04 00:42:01 -0400

    Toggle English and Spanish NavItem

--- src/components/navbar.tsx
+++ src/components/navbar.tsx
@@ -1,7 +1,11 @@
+import { useState } from 'react';
 import '../styles/navbar.css';
 import { Link } from 'react-router-dom';
 
 function Navbar() {
+    const [showingDropDown, setShowingDropDown] = useState(false);
+    const [language, setLanguage] = useState('EN');
+
     function MobileNavAnimation() {
         const hamburgerMenu = document.getElementById('menuCheck') as HTMLInputElement;
         const mobileNav = document.getElementById('hamburgerNav') as HTMLInputElement;
@@ -22,6 +26,38 @@ function Navbar() {
         mobileNav.style.translate = '-100%';
     }
 
+    function showDropdown() {
+        const dropDownMenu = document.getElementById('dropDownLanguage') as HTMLElement;
+
+        if (!showingDropDown) {
+            setShowingDropDown(true);
+            dropDownMenu.style.translate = '0';
+        } else {
+            dropDownMenu.style.translate = '0 -250%';
+            setShowingDropDown(false);
+        }
+    }
+
+    function toggleEnglish() {
+        const indicator = document.getElementById('toggleLanguage') as HTMLElement;
+
+        if (language !== 'EN') {
+            setLanguage('EN');
+            showDropdown();
+            indicator.innerHTML = 'EN';
+        }
+    }
+
+    function toggleSpanish() {
+        const indicator = document.getElementById('toggleLanguage') as HTMLElement;
+
+        if (language !== 'ES') {
+            setLanguage('ES');
+            showDropdown();
+            indicator.innerHTML = 'ES';
+        }
+    }
+
     return (
         <>
             <div id='wrapper'>
@@ -40,10 +76,15 @@ function Navbar() {
                     <Link to='/services' className='navItem'>Services</Link>
                     <Link to='/portfolio' className='navItem'>Portfolio</Link>
                     <Link to='/contact' className='navItem'>Contact</Link>
+                    <h2 id='toggleLanguage' onClick={showDropdown}>EN</h2>
                     <label id='hamburgerMenu' onClick={MobileNavAnimation}>
                         <input id='menuCheck' type='checkbox'/>
                     </label>
                 </nav>
+            </div>
+            <div id='dropDownLanguage'>
+                <h2 className='dropItem' onClick={toggleEnglish}>English</h2>
+                <h2 className='dropItem' onClick={toggleSpanish}>Español</h2>
             </div>
             <div id='hamburgerNav'>
                 <Link to='/' className='mobileNavLink' onClick={closeNav}>Home</Link>
